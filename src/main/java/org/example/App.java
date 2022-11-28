0def9e80105809e8d46efead860a1d2d07cbe48f
Ruslan <Ruslan90.0843@gmail.com>
2022-11-28 15:45:07 +0400

    End Task_01

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 /**
  * 1.0. Максимальное, минимальное и среднее значение
  * Задача:
@@ -8,6 +10,53 @@ package org.example;
  */
 public class App {
     public static void main(String[] args) {
+        int[] arrays = fillArrays();
+        System.out.println(Arrays.toString(arrays));
+        serchMaxMin(arrays);
+    }
 
+    private static int randomSize() {
+        return (int) (Math.random() * 10);
+    }
+
+    private static int[] fillArrays() {
+        int[] arr = new int[randomSize()];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 10);
+        }
+        return arr;
+    }
+
+    private static void serchMaxMin(int[] arr) {
+        int max, min;
+        if (arr.length == 0) {
+            System.out.println("Длина массива равна =" + arr.length);
+            return;
+        } else if (arr.length == 1) {
+            min = arr[0];
+            max = arr[0];
+        } else {
+            max = arr[0];
+            min = arr[1];
+            if (max < min) {
+                int var = max;
+                max = min;
+                min = var;
+            }
+
+            for (int i = 2; i < arr.length; i++) {
+                if (max < arr[i]) {
+                    int var = max;
+                    max = arr[i];
+                    arr[i] = var;
+                } else if (min > arr[i]) {
+                    int var = min;
+                    min = arr[i];
+                    arr[i] = var;
+                }
+            }
+        }
+        System.out.println("Минимальное число равно = " + min);
+        System.out.println("Максимальное число равно = " + max);
     }
 }
